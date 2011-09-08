ce026fcf85296bbc58989e38d0844faac2279137
mihailik <mihailik@gmail.com>
2011-09-08 08:56:09 +0100

    Adding tests: created from VS.

---
+++ Mi.PE.Tests/PEFileReaderTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mi.PE.Tests
+{
+    [TestClass]
+    public class PEFileReaderTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
