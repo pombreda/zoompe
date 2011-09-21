96a8fff99502dfde91935b37d229ff82100001a8
mihailik <mihailik@gmail.com>
2011-09-21 09:24:38 +0100

    Added PEFileTests for testing coordination between PEHeader.NumberOfSections and PEFile.Sections collection.

---
+++ (Tests)/Mi.PE.Tests/PEFileTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mi.PE
+{
+    [TestClass]
+    public class PEFileTests
+    {
+        [TestMethod]
+        public void SettingPEHeaderNumberOfSections_ChangesSectionsCount()
+        {
+            var pe = new PEFile();
+            Assert.AreEqual(0, pe.Sections.Count);
+            pe.PEHeader.NumberOfSections = 3;
+            Assert.AreEqual(3, pe.Sections.Count);
+        }
+
+        [TestMethod]
+        public void SettingPEHeaderNumberOfSections_ClearsSections()
+        {
+            var pe = new PEFile();
+            pe.PEHeader.NumberOfSections = 1;
+            pe.Sections[0].Name = "dummy";
+            pe.PEHeader.NumberOfSections = 3;
+            Assert.AreEqual(null, pe.Sections[0].Name);
+        }
+
+        [TestMethod]
+        public void SettingSamePEHeaderNumberOfSections_LeavesSectionsIntact()
+        {
+            var pe = new PEFile();
+            pe.PEHeader.NumberOfSections = 1;
+            pe.Sections[0].Name = "dummy";
+            pe.PEHeader.NumberOfSections = 1;
+            Assert.AreEqual("dummy", pe.Sections[0].Name);
+        }
+    }
+}
