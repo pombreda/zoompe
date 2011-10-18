d35bd6deb310bd72da6a4ed3f1f69cfe53bf809c
mihailik <mihailik@gmail.com>
2011-10-18 15:59:47 +0100

    Added PEHeaderModel.

---
+++ Zoom.PE.SL/Model/PEHeaderModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Mi.PE;
+using Mi.PE.PEFormat;
+
+namespace Zoom.PE.Model
+{
+    public sealed class PEHeaderModel : PEFilePartModel
+    {
+        readonly PEHeader peHeader;
+
+        public PEHeaderModel(PEHeader peHeader, DosHeaderModel dosHeaderModel)
+        {
+            this.peHeader = peHeader;
+
+            this.Address = dosHeaderModel.lfanew;
+            this.Length = PEHeader.Size;
+
+            BindAddressToDosHeaderlfanew(dosHeaderModel);
+        }
+
+        public Machine Machine
+        {
+            get { return peHeader.Machine; }
+            set
+            {
+                if (value == peHeader.Machine)
+                    return;
+
+                peHeader.Machine = value;
+                OnPropertyChanged("Machine");
+            }
+        }
+
+        public ushort NumberOfSections
+        {
+            get { return peHeader.NumberOfSections; }
+            set
+            {
+                if (value == peHeader.NumberOfSections)
+                    return;
+
+                peHeader.NumberOfSections = value;
+                OnPropertyChanged("NumberOfSections");
+            }
+        }
+        
+        public DateTime Timestamp
+        {
+            get { return peHeader.Timestamp; }
+            set
+            {
+                if (value == peHeader.Timestamp)
+                    return;
+
+                peHeader.Timestamp = value;
+                OnPropertyChanged("Timestamp");
+            }
+        }
+        
+        public uint PointerToSymbolTable
+        {
+            get { return peHeader.PointerToSymbolTable; }
+            set
+            {
+                if (value == peHeader.PointerToSymbolTable)
+                    return;
+
+                peHeader.PointerToSymbolTable = value;
+                OnPropertyChanged("PointerToSymbolTable");
+            }
+        }
+        
+        public uint NumberOfSymbols
+        {
+            get { return peHeader.NumberOfSymbols; }
+            set
+            {
+                if (value == peHeader.NumberOfSymbols)
+                    return;
+
+                peHeader.NumberOfSymbols = value;
+                OnPropertyChanged("NumberOfSymbols");
+            }
+        }
+        
+        public ushort SizeOfOptionalHeader
+        {
+            get { return peHeader.SizeOfOptionalHeader; }
+            set
+            {
+                if (value == peHeader.SizeOfOptionalHeader)
+                    return;
+
+                peHeader.SizeOfOptionalHeader = value;
+                OnPropertyChanged("SizeOfOptionalHeader");
+            }
+        }
+        
+        public ImageCharacteristics Characteristics
+        {
+            get { return peHeader.Characteristics; }
+            set
+            {
+                if (value == peHeader.Characteristics)
+                    return;
+
+                peHeader.Characteristics = value;
+                OnPropertyChanged("Characteristics");
+            }
+        }
+
+        private void BindAddressToDosHeaderlfanew(DosHeaderModel dosHeaderModel)
+        {
+            dosHeaderModel.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == "lfanew")
+                    this.Address = dosHeaderModel.lfanew;
+            };
+        }
+    }
+}
