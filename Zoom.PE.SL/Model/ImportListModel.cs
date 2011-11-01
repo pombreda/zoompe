3b681527689f42d937dadddbde1605995fac8b9c
mihailik <mihailik@gmail.com>
2011-11-01 18:32:36 +0000

    DLL Import ViewModel classes.

---
+++ Zoom.PE.SL/Model/ImportListModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using Mi.PE;
+using Mi.PE.PEFormat;
+
+namespace Zoom.PE.Model
+{
+    public sealed class ImportListModel : ReadOnlyObservableCollection<FunctionImportModel>
+    {
+        public ImportListModel()
+            : base(new ObservableCollection<FunctionImportModel>())
+        {
+        }
+
+        internal void Add(FunctionImportModel item)
+        {
+            this.Items.Add(item);
+        }
+
+        internal void Remove(FunctionImportModel item)
+        {
+            this.Items.Remove(item);
+        }
+
+        internal void Clear()
+        {
+            this.Items.Clear();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string propertyName)
+        {
+            var propertyChangedHandler = this.PropertyChanged;
+            if (propertyChangedHandler != null)
+                propertyChangedHandler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
