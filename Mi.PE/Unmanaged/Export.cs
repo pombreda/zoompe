fea65421f61e3e41f849af1ff134cc8123e44959
mihailik <mihailik@gmail.com>
2011-11-03 09:55:22 +0000

    Adding dll Export.

---
+++ Mi.PE/Unmanaged/Export.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mi.PE.Unmanaged
+{
+    public sealed class Export
+    {
+        public sealed class Header
+        {
+            public uint Flags;
+
+        }
+
+        public string FunctionName;
+        public uint FunctionOrdinal;
+        public uint Pointer;
+        public string Forwarder;
+    }
+}
