06a521ce0182208eef3e6e4cca784e12567131cd
mihailik <mihailik@gmail.com>
2011-11-14 19:05:29 +0000

    Renaming *RelocationType to just *.

--- Mi.PE/Unmanaged/RelocationType.cs
+++ Mi.PE/Unmanaged/RelocationType.cs
@@ -19,15 +19,15 @@ namespace Mi.PE.Unmanaged
         }
 
         public static implicit operator RelocationType(Amd64 value) { return new RelocationType((short)value); }
-        public static implicit operator RelocationType(ArmRelocationType value) { return new RelocationType((short)value); }
-        public static implicit operator RelocationType(HitachiSuperHRelocationType value) { return new RelocationType((short)value); }
+        public static implicit operator RelocationType(Arm value) { return new RelocationType((short)value); }
+        public static implicit operator RelocationType(HitachiSuperH value) { return new RelocationType((short)value); }
         public static implicit operator RelocationType(PowerPCRelocationType value) { return new RelocationType((short)value); }
         public static implicit operator RelocationType(Amd64 value) { return new RelocationType((short)value); }
 
 
         public static implicit operator Amd64(RelocationType value) { return (Amd64)value.value; }
-        public static implicit operator ArmRelocationType(RelocationType value) { return (ArmRelocationType)value.value; }
-        public static implicit operator HitachiSuperHRelocationType(RelocationType value) { return (HitachiSuperHRelocationType)value.value; }
+        public static implicit operator Arm(RelocationType value) { return (Arm)value.value; }
+        public static implicit operator HitachiSuperH(RelocationType value) { return (HitachiSuperH)value.value; }
         public static implicit operator PowerPCRelocationType(RelocationType value) { return (PowerPCRelocationType)value.value; }
         public static implicit operator Amd64(RelocationType value) { return new (Amd64)value.value; }
     }
