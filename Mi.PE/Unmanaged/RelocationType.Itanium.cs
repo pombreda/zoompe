c5b3618b55bd18f2c08abbe1dd5252088f3e96fa
mihailik <mihailik@gmail.com>
2011-11-16 09:32:08 +0000

    Adding Itanium conversion operators in RelocationType.

--- Mi.PE/Unmanaged/RelocationType.Itanium.cs
+++ Mi.PE/Unmanaged/RelocationType.Itanium.cs
@@ -11,28 +11,28 @@ namespace Mi.PE.Unmanaged
             /// <summary>
             /// The relocation is ignored.
             /// </summary>
-            Absolute= 0x000,
+            Absolute = 0x000,
 
             /// <summary>
             /// The instruction relocation can be followed by an <see cref="AddEnd"/> relocation
             /// whose value is added to the target address before it is inserted into the specified slot in the <see cref="IMM14"/> bundle.
             /// The relocation target must be absolute or the image must be fixed.
             /// </summary>
-            IMM14= 0x001,
+            IMM14 = 0x001,
 
             /// <summary>
             /// The instruction relocation can be followed by an <see cref="AddEnd"/> relocation
             /// whose value is added to the target address before it is inserted into the specified slot in the <see cref="IMM22"/> bundle.
             /// The relocation target must be absolute or the image must be fixed.
             /// </summary>
-            IMM22= 0x002,
+            IMM22 = 0x002,
 
             /// <summary>
             /// The slot number of this relocation must be one (1).
             /// The relocation can be followed by an <see cref="AddEnd"/> relocation
             /// whose value is added to the target address before it is stored in all three slots of the <see cref="IMM64"/> bundle.
             /// </summary>
-            IMM64= 0x003,
+            IMM64 = 0x003,
 
             /// <summary>
             /// The target’s 32-bit VA. This is supported only for /LARGEADDRESSAWARE:NO images.
@@ -61,33 +61,33 @@ namespace Mi.PE.Unmanaged
             /// The bundle is fixed up with the 25-bit relative displacement of the 16-bit aligned target.
             /// The low 4 bits of the displacement are zero and are not stored
             /// </summary>
-            PCRel21F= 0x008,
-            
+            PCRel21F = 0x008,
+
             /// <summary>
             /// The instruction relocation can be followed by an <see cref="AddEnd"/> relocation
             /// whose value is added to the target address and then a 22-bit GPrelative offset
             /// that is calculated and applied to the <see cref="GPRel22"/> bundle.
             /// </summary>
-            GPRel22= 0x009,
+            GPRel22 = 0x009,
 
             /// <summary>
             /// The instruction is fixed up with the 22-bit GPrelative offset to the target symbol’s literal table entry.
             /// The linker creates this literal table entry based on this relocation and the <see cref="AddEnd"/> relocation that might follow.
             /// </summary>
-            LTOff22= 0x00A,
-            
+            LTOff22 = 0x00A,
+
             /// <summary>
             /// The 16-bit section index of the section contains the target.
             /// This is used to support debugging information.
             /// </summary>
-            Section= 0x00B,
-            
+            Section = 0x00B,
+
             /// <summary>
             /// The instruction is fixed up with the 22-bit offset of the target from the beginning of its section.
             /// This relocation can be followed immediately by an <see cref="AddEnd"/> relocation,
             /// whose Value field contains the 32-bit unsigned offset of the target from the beginning of the section.
             /// </summary>
-            SecRel22= 0x00C,
+            SecRel22 = 0x00C,
 
             /// <summary>
             /// The slot number for this relocation must be one (1).
@@ -95,62 +95,62 @@ namespace Mi.PE.Unmanaged
             /// This relocation can be followed immediately by an <see cref="AddEnd"/> relocation,
             /// whose Value field contains the 32-bit unsigned offset of the target from the beginning of the section.
             /// </summary>
-            SecRel64I= 0x00D,
+            SecRel64I = 0x00D,
 
             /// <summary>
             /// The address of data to be fixed up with the 32-bit offset of the target from the beginning of its section.
             /// </summary>
-            SecRel32= 0x00E,
+            SecRel32 = 0x00E,
 
             /// <summary>
             /// The target’s 32-bit RVA.
             /// </summary>
-            Dir32NB= 0x010,
+            Dir32NB = 0x010,
 
             /// <summary>
             /// This is applied to a signed 14-bit immediate that contains the difference between two relocatable targets.
             /// This is a declarative field for the linker that indicates that the compiler has already emitted this value.
             /// </summary>
-            SRel14= 0x011,
+            SRel14 = 0x011,
 
             /// <summary>
             /// This is applied to a signed 22-bit immediate that contains the difference between two relocatable targets.
             /// This is a declarative field for the linker that indicates that the compiler has already emitted this value.
             /// </summary>
-            SRel22= 0x012,
+            SRel22 = 0x012,
 
             /// <summary>
             /// This is applied to a signed 32-bit immediate that contains the difference between two relocatable values.
             /// This is a declarative field for the linker that indicates that the compiler has already emitted this value.
             /// </summary>
-            SRel32= 0x013,
+            SRel32 = 0x013,
 
             /// <summary>
             /// This is applied to an unsigned 32-bit immediate that contains the difference between two relocatable values.
             /// This is a declarative field for the linker that indicates that the compiler has already emitted this value.
             /// </summary>
-            URel32= 0x014,
-            
+            URel32 = 0x014,
+
             /// <summary>
             /// A 60-bit PC-relative fixup that always stays as a BRL instruction of an MLX bundle.
             /// </summary>
-            PCRel60X= 0x015,
-            
+            PCRel60X = 0x015,
+
             /// <summary>
             /// A 60-bit PC-relative fixup.
             /// If the target displacement fits in a signed 25-bit field, convert the entire bundle to an MBB bundle
             /// with NOP.B in slot 1 and a 25-bit BR instruction
             /// (with the 4 lowest bits all zero and dropped) in slot 2.
             /// </summary>
-            PCRel60B= 0x016,
-            
+            PCRel60B = 0x016,
+
             /// <summary>
             /// A 60-bit PC-relative fixup.
             /// If the target displacement fits in a signed 25-bit field, convert the entire bundle to an MFB bundle
             /// with NOP.F in slot 1 and a 25-bit
             /// (4 lowest bits all zero and dropped) BR instruction in slot 2.
             /// </summary>
-            PCRel60F= 0x017,
+            PCRel60F = 0x017,
 
             /// <summary>
             /// A 60-bit PC-relative fixup.
@@ -158,30 +158,30 @@ namespace Mi.PE.Unmanaged
             /// with NOP.I in slot 1 and a 25-bit
             /// (4 lowest bits all zero and dropped) BR instruction in slot 2.
             /// </summary>
-            PCRel60I= 0x018,
-            
+            PCRel60I = 0x018,
+
             /// <summary>
             /// A 60-bit PC-relative fixup.
             /// If the target displacement fits in a signed 25-bit field, convert the entire bundle to an MMB bundle
             /// with NOP.M in slot 1 and a 25-bit
             /// (4 lowest bits all zero and dropped) BR instruction in slot 2.
             /// </summary>
-            PCRel60M= 0x019,
-            
+            PCRel60M = 0x019,
+
             /// <summary>
             /// A 64-bit GP-relative fixup.
             /// </summary>
-            IMMGPRel64= 0x01a,
+            IMMGPRel64 = 0x01a,
 
             /// <summary>
             /// A CLR token.
             /// </summary>
-            TOKEN= 0x01b,
+            Token = 0x01b,
 
             /// <summary>
             /// A 32-bit GP-relative fixup.
             /// </summary>
-            GPRel32= 0x01c,
+            GPRel32 = 0x01c,
 
             /// <summary>
             /// The relocation is valid only when it immediately follows one of the following relocations:
@@ -189,7 +189,7 @@ namespace Mi.PE.Unmanaged
             /// <see cref="SecRel22"/>, <see cref="SecRel64I"/>, or <see cref="SecRel32"/>.
             /// Its value contains the <see cref="AddEnd"/> to apply to instructions within a bundle, not for data.
             /// </summary>
-            AddEnd= 0x01F
-}
+            AddEnd = 0x01F
+        }
     }
 }
--- Mi.PE/Unmanaged/RelocationType.cs
+++ Mi.PE/Unmanaged/RelocationType.cs
@@ -23,6 +23,7 @@ namespace Mi.PE.Unmanaged
         public static implicit operator RelocationType(HitachiSuperH value) { return new RelocationType((short)value); }
         public static implicit operator RelocationType(PowerPC value) { return new RelocationType((short)value); }
         public static implicit operator RelocationType(I386 value) { return new RelocationType((short)value); }
+        public static implicit operator RelocationType(Itanium value) { return new RelocationType((short)value); }
 
 
         public static implicit operator Amd64(RelocationType value) { return (Amd64)value.value; }
@@ -30,5 +31,6 @@ namespace Mi.PE.Unmanaged
         public static implicit operator HitachiSuperH(RelocationType value) { return (HitachiSuperH)value.value; }
         public static implicit operator PowerPC(RelocationType value) { return (PowerPC)value.value; }
         public static implicit operator I386(RelocationType value) { return (I386)value.value; }
+        public static implicit operator Itanium(RelocationType value) { return (Itanium)value.value; }
     }
 }
