cb4000976e83362179003d36c94edc247dd33342
mihailik <mihailik@gmail.com>
2011-11-17 22:31:27 +0000

    Moved final part of ClrHeaderReader into TableStream and deleted the empty file.

--- Mi.PE/Cli/TableStream.cs
+++ Mi.PE/Cli/TableStream.cs
@@ -5,6 +5,8 @@ using System.Text;
 
 namespace Mi.PE.Cli
 {
+    using Mi.PE.Internal;
+
     public sealed class TableStream
     {
         public int Reserved0 { get; set; }
@@ -15,5 +17,39 @@ namespace Mi.PE.Cli
         public ulong Valid { get; set; }
         public ulong Sorted { get; set; }
         public uint[] Rows { get; set; }
+
+        public void Read(BinaryStreamReader reader)
+        {
+            this.Reserved0 = reader.ReadInt32();
+            this.MajorVersion = reader.ReadByte();
+            this.MinorVersion = reader.ReadByte();
+            this.HeapSizes = reader.ReadByte();
+            this.Reserved1 = reader.ReadByte();
+            this.Valid = reader.ReadUInt64();
+            this.Sorted = reader.ReadUInt64();
+
+            uint[] rows = new uint[CountNonzeroBits(this.Valid)];
+
+            for (int i = 0; i < rows.Length; i++)
+            {
+                rows[i] = reader.ReadUInt32();
+            }
+
+            this.Rows = rows;
+        }
+
+        private static int CountNonzeroBits(ulong x)
+        {
+            int tableCount = 0;
+            while (x != 0)
+            {
+                if ((x & 1) != 0)
+                    tableCount++;
+
+                x = x / 2;
+            }
+            return tableCount;
+        }
+
     }
 }
