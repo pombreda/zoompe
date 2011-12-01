875891c217c022b2eb69558c1ff04f1782812d86
mihailik <mihailik@gmail.com>
2011-12-01 08:17:48 +0000

    MemberRefEntry (fields, comments) and MemberRefParent 'coded index' struct.

---
+++ Mi.PE/Cli/Tables/MemberRefEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mi.PE.Cli.Tables
+{
+    /// <summary>
+    /// The <see cref="TableKind.MemberRef"/> table combines two sorts of references, to Methods and to Fields of a class,
+    /// known as 'MethodRef' and 'FieldRef', respectively.
+    /// [ECMA 22.25]
+    /// </summary>
+    public sealed class MemberRefEntry
+    {
+        /// <summary>
+        /// An index into the <see cref="TabeKind.MethodDef"/>, <see cref="TableKind.ModuleRef"/>,
+        /// <see cref="TableKind.TypeDef"/>, <see cref="TableKind.TypeRef"/>, or <see cref="TableKind.TypeSpec"/> tables;
+        /// more precisely, a <see cref="MemberRefParent"/> (ECMA §24.2.6) coded index.
+        /// </summary>
+        /// <remarks>
+        /// An entry is made into the <see cref="TableKind.MemberRef"/> table
+        /// whenever a reference is made in the CIL code to a method or field
+        /// which is defined in another module or assembly.
+        /// (Also, an entry is made for a call to a method with a VARARG signature, even when it is defined in the same module as the call site.)
+        /// </remarks>
+        public MemberRefParent Class;
+
+        public string Name;
+
+        public byte[] Signature;
+    }
+}
---
+++ Mi.PE/Cli/Tables/MemberRefParent.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mi.PE.Cli.Tables
+{
+    public struct MemberRefParent
+    {
+        public enum TableKind
+        {
+            TypeDef = 0,
+            TypeRef = 1,
+            ModuleRef = 2,
+            MethodDef = 3,
+            TypeSpec = 4
+        }
+
+        const int HighBitCount = 3;
+
+        const uint WideKindMask = uint.MaxValue << HighBitCount;
+        const ushort NarrowKindMask = unchecked((ushort)(ushort.MaxValue << HighBitCount));
+
+        readonly uint value;
+
+        private MemberRefParent(uint value)
+        {
+            this.value = value;
+        }
+
+        public TableKind Kind { get { return (TableKind)(value >> (32 - HighBitCount)); } }
+        public uint Index { get { return value & ~WideKindMask; } }
+
+        public static explicit operator MemberRefParent(uint value)
+        {
+            return new MemberRefParent(value);
+        }
+
+        public static explicit operator MemberRefParent(ushort value)
+        {
+            ushort high = (ushort)(value & NarrowKindMask);
+            ushort low = (ushort)(value & ~NarrowKindMask);
+
+            uint extended = (uint)((high << 16) | low);
+
+            return new MemberRefParent(extended);
+        }
+
+        public static explicit operator uint(MemberRefParent value)
+        {
+            return value.value;
+        }
+
+        public static explicit operator ushort(MemberRefParent value)
+        {
+            ushort high = (ushort)(value.value >> 16);
+            ushort low = (ushort)(value.value & ushort.MaxValue);
+
+            ushort compacted = (ushort)(high | low);
+
+            return compacted;
+        }
+    }
+}
