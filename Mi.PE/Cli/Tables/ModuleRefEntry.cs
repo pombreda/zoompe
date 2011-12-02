588d7666687557fbd1ade52793f247b834c75e4f
mihailik <mihailik@gmail.com>
2011-12-02 14:24:59 +0000

    ModuleRefEntry: fields, comments and Read method too.

---
+++ Mi.PE/Cli/Tables/ModuleRefEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mi.PE.Cli.Tables
+{
+    /// <summary>
+    /// The rows in the ModuleRef table result from .module extern directives in the Assembly (ECMA §6.5).
+    /// [ECMA 22.31]
+    /// </summary>
+    public sealed class ModuleRefEntry
+    {
+        public string Name;
+
+        public void Read(ClrModuleReader reader)
+        {
+            this.Name = reader.ReadString();
+        }
+    }
+}
