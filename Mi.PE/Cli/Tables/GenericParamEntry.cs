fe321663c291500d0e7ac1853aa9e884cb95cbb7
mihailik <mihailik@gmail.com>
2011-12-03 23:30:25 +0000

    More comments in GenericParamEntry.

--- Mi.PE/Cli/Tables/GenericParamEntry.cs
+++ Mi.PE/Cli/Tables/GenericParamEntry.cs
@@ -5,6 +5,13 @@ using System.Linq;
 namespace Mi.PE.Cli.Tables
 {
     /// <summary>
+    /// The <see cref="TableKind.GenericParam"/> table stores the generic parameters used in generic type definitions and generic method definitions.
+    /// These generic parameters can be constrained
+    /// (i.e., generic arguments shall extend some class and/or implement certain interfaces)
+    /// or unconstrained.
+    /// (Such constraints are stored in the <see cref="TableKind.GenericParamConstraint"/> table.)
+    /// Conceptually, each row in the <see cref="TableKind.GenericParam"/> table is owned by one, and only one, row
+    /// in either the <see cref="TableKind.TypeDef"/> or <see cref="TableKind.MethodDef"/> tables.
     /// [ECMA 22.20]
     /// </summary>
     public sealed class GenericParamEntry
@@ -24,11 +31,26 @@ namespace Mi.PE.Cli.Tables
         /// specifying the Type or Method to which this generic parameter applies;
         /// more precisely, a <see cref="TypeOrMethodDef"/> (ECMA §24.2.6) coded index.
         /// </summary>
+        /// <remarks>
+        /// The following additional restrictions apply:
+        /// * <see cref="Owner"/> cannot be a non nested enumeration type; and
+        /// * If <see cref="Owner"/> is a nested enumeration type then Number must be less than or equal
+        /// to the number of generic parameters of the enclosing class.
+        /// Rationale: Generic enumeration types serve little purpose and usually only exist to meet CLR Rule 42.
+        /// These additional restrictions limit the genericty of enumeration types while allowing CLS Rule 42 to be met.
+        /// </remarks>
         public TypeOrMethodDef Owner;
 
         /// <summary>
         /// This is purely descriptive and is used only by source language compilers and by Reflection.
         /// </summary>
         public string Name;
+
+        public void Read(ClrModuleReader reader)
+        {
+            this.Number = reader.Binary.ReadUInt16();
+            this.Owner = reader.ReadTypeOrMethodDef();
+            this.Name = reader.ReadString();
+        }
     }
 }
