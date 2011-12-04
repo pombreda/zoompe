f5b49121c6fb06dccabf3fa9f5ce8249e1d3fe72
mihailik <mihailik@gmail.com>
2011-12-04 12:06:19 +0000

    GenericParamConstraintEntry: fields and comments, no Read method.

---
+++ Mi.PE/Cli/Tables/GenericParamConstraintEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mi.PE.Cli.Tables
+{
+    /// <summary>
+    /// [ECMA 22.21]
+    /// </summary>
+    public sealed class GenericParamConstraintEntry
+    {
+        /// <summary>
+        /// An index into the <see cref="TableKind.GenericParam"/> table, specifying to which generic parameter this row refers.
+        /// </summary>
+        public uint Owner;
+
+        /// <summary>
+        /// An index into the <see cref="TableKind.TypeDef"/>, <see cref="TableKind.TypeRef"/>, or <see cref="TableKind.TypeSpec"/> tables,
+        /// specifying from which class this generic parameter is constrained to derive;
+        /// or which interface this generic parameter is constrained to implement;
+        /// more precisely, a <see cref="TypeDefOrRef"/> (ECMA §24.2.6) coded index.
+        /// </summary>
+        public TypeDefOrRef Constraint;
+    }
+}
