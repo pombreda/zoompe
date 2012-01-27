97535d9189906548ae3e2d42a76b3f1ca914d319
mihailik <mihailik@gmail.com>
2012-01-27 20:42:09 +0000

    CustomMod and RefType added in Signatures namespace.

---
+++ Mi.PE/Cli/Signatures/CustomMod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mi.PE.Cli.Signatures
+{
+    /// <summary>
+    /// [ECMA-335 §23.2.7]
+    /// </summary>
+    public sealed class CustomMod
+    {
+    }
+}
---
+++ Mi.PE/Cli/Signatures/RefType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mi.PE.Cli.Signatures
+{
+    using Mi.PE.Internal;
+
+    /// <summary>
+    /// [ECMA-335 §23.2.11]
+    /// </summary>
+    public sealed class RefType
+    {
+        public void Read(BinaryStreamReader signatureBlobReader)
+        {
+            var etype = (ElementType)signatureBlobReader.ReadByte();
+            if (etype == ElementType.ByRef)
+            {
+            }
+            else if (etype == ElementType.TypedBeRef)
+            {
+            }
+            else if(etype == ElementType.Void)
+            {
+            }
+        }
+    }
+}
